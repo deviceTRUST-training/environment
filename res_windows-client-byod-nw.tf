728c1e9db2d6302a813b8a2af0ee9b467276b09c
Sven Jansen <sven.jansen@devicetrust.com>
2022-12-19 22:55:26 +0100

    .

--- res_windows-client-byod-nw.tf
+++ res_windows-client-byod-nw.tf
@@ -13,7 +13,6 @@ resource "azurerm_network_interface" "vm_byod" {
   name                = "${var.azure-environment.prefix}_vm_byod_nic"
   location            = "${element(azurerm_resource_group.main.*.location, count.index)}"
   resource_group_name = "${element(azurerm_resource_group.main.*.name, count.index)}"
-  dns_servers         = ["10.10.11.10"]
   tags                = "${var.tags}"
 
   ip_configuration {

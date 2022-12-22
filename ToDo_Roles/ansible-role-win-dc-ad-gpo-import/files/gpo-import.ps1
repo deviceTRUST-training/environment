53dad5c52912a468b6b133dca1b6afa58fc23455
Sven Jansen <sven.jansen@devicetrust.com>
2022-12-22 19:57:28 +0100

    .

--- ToDo_Roles/ansible-role-win-dc-ad-gpo-import/files/gpo-import.ps1
+++
@@ -1,9 +0,0 @@
-$BackupPath = "c:\GPO\GPO"
-
-Get-Childitem $BackupPath | ForEach-Object{
-
-    $BackupGUID = (Get-ChildItem $_.FullName).Name.split(" ")[0].Replace("{","").Replace("}","")
-
-    Import-GPO -BackupId $BackupGUID -Path $_.FullName -TargetName $_.Name -CreateIfNeeded
-
-}

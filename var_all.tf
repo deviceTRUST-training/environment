3ac70cfc92ebf3d88b5a0420432b9d5522f53390
dTSvenJansen <sven.jansen@devicetrust.com>
2023-05-22 14:21:45 +0200

    -

--- var_all.tf
+++ var_all.tf
@@ -5,7 +5,7 @@ variable azure-environment {
     subscription_id      = "1087b067-72b6-4f7f-9999-a9c3172a9ae4"
     tenant_id            = "07c7e7f5-210d-4fc6-bd6d-929caf1e3b61"
     client_id            = "4eec80f4-cb7a-44fb-b4e8-3c3a8b826a22"
-    client_secret        = "oMb8Q~3YXMwVoKJUrvG1sGjpWsRo7orzyTuClduB" # Expires 3/9/2023
+    client_secret        = "Bth8Q~qCXjCfGENn5NoSY5Mb8wpgcXNSmI4u5cbh" # 
     prefix               = "training"
     location             = "West Europe"
     instance_count       = "1"

9a2a8a3958fc1710f5a80c66444a934ac44c7758
Sven Jansen <sven.jansen@devicetrust.com>
2023-05-24 12:25:40 +0200

    .

--- res_ubuntu-server-controller.tf
+++ res_ubuntu-server-controller.tf
@@ -73,7 +73,7 @@ resource "azurerm_virtual_machine" "vm_controller" {
       "cd /tmp",
       "cd /tmp/ansible",
       "ansible-galaxy install -v -r requirements.yml -p ./roles/",
-      "ansible-playbook -i inventory create-env.yml --extra-vars 'ansible_user=${var.vm.username} ansible_password=${var.vm.password} domain_admin_user=${var.vm.username}@${var.vm.domain_dns_name} domain_admin_password=${var.vm.password} safe_mode_password=${var.vm.password}'",
+      "ansible-playbook -i inventory site.yml --extra-vars 'ansible_user=${var.vm.username} ansible_password=${var.vm.password} domain_admin_user=${var.vm.username}@${var.vm.domain_dns_name} domain_admin_password=${var.vm.password} safe_mode_password=${var.vm.password}'",
     ]
   }
 

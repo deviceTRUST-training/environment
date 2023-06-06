8bd044322f04ec04f5816b9c4d44ff82309815ef
Sven Jansen <sven.jansen@devicetrust.com>
2023-06-06 23:17:08 +0200

    .

--- output.tf
+++ output.tf
@@ -1,39 +1,49 @@
-output "public_ip_address_vm_controller" {
-  value = "${azurerm_public_ip.vm_controller.*.ip_address}"
-}
 output "fqdn_vm_controller" {
   value = "${azurerm_public_ip.vm_controller.*.fqdn}"
 }
-
-output "public_ip_address_vm_dc" {
-  value = "${azurerm_public_ip.vm_dc.*.ip_address}"
+output "private_ip_address_vm_controller" {
+  value = "${azurerm_network_interface.vm_controller.*.ip_configuration.private_ip_address}"
 }
+output "public_ip_address_vm_controller" {
+  value = "${azurerm_public_ip.vm_controller.*.ip_address}"
+}
+
 output "fqdn_vm_dc" {
   value = "${azurerm_public_ip.vm_dc.*.fqdn}"
 }
+output "private_ip_address_vm_dc" {
+  value = "${azurerm_network_interface.vm_dc.*.ip_configuration.private_ip_address}"
+}
+output "public_ip_address_vm_dc" {
+  value = "${azurerm_public_ip.vm_dc.*.ip_address}"
+}
 
-output "public_ip_address_vm_rdsh" {
-  value = "${azurerm_public_ip.vm_rdsh.*.ip_address}"
-}
-output "fqdn_vm_rdsh" {
-  value = "${azurerm_public_ip.vm_rdsh.*.fqdn}"
-}
-
-output "public_ip_address_vm_client" {
-  value = "${azurerm_public_ip.vm_client.*.ip_address}"
-}
 output "fqdn_vm_client" {
   value = "${azurerm_public_ip.vm_client.*.fqdn}"
 }
-
-output "public_ip_address_vm_byod" {
-  value = "${azurerm_public_ip.vm_byod.*.ip_address}"
+output "private_ip_address_vm_client" {
+  value = "${azurerm_network_interface.vm_client.*.ip_configuration.private_ip_address}"
+}
+output "public_ip_address_vm_client" {
+  value = "${azurerm_public_ip.vm_client.*.ip_address}"
 }
 
 output "fqdn_vm_byod" {
   value = "${azurerm_public_ip.vm_byod.*.fqdn}"
 }
+output "private_ip_address_vm_byod" {
+  value = "${azurerm_network_interface.vm_byod.*.ip_configuration.private_ip_address}"
+}
+output "public_ip_address_vm_byod" {
+  value = "${azurerm_public_ip.vm_byod.*.ip_address}"
+}
 
-#output "ssh_command" {
-#  value = "ssh ${local.admin_username}@${azurerm_public_ip.main.*.ip_address}"
-#}
+output "fqdn_vm_rdsh" {
+  value = "${azurerm_public_ip.vm_rdsh.*.fqdn}"
+}
+output "private_ip_address_vm_rdsh" {
+  value = "${azurerm_network_interface.vm_rdsh.*.ip_configuration.private_ip_address}"
+}
+output "public_ip_address_vm_rdsh" {
+  value = "${azurerm_public_ip.vm_rdsh.*.ip_address}"
+}

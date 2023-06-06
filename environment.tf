e4628b0b954346a85f43d21ee4a5ae09875e1ef3
Sven Jansen <sven.jansen@devicetrust.com>
2023-06-06 22:27:28 +0200

    .

--- environment.tf
+++ environment.tf
@@ -1,13 +1,14 @@
 # generate inventory file for Ansible
 resource "local_file" "hosts_cfg" {
-  content = templatefile("${path.module}/ansible/templates/hosts.tpl",
+  content           = templatefile("${path.module}/ansible/templates/hosts.tpl",
     {
-      controller = azurerm_network_interface.vm_controller.*.ip_configuration.private_ip_address
-      dc = azurerm_network_interface.vm_dc.*.ip_configuration.private_ip_address
-      rdsh = azurerm_network_interface.vm_rdsh.*.ip_configuration.private_ip_address
-      client = azurerm_network_interface.vm_client.*.ip_configuration.private_ip_address
-      byod = azurerm_network_interface.vm_byod.*.ip_configuration.private_ip_address
+      # controller = azurerm_network_interface.vm_controller.*.ip_configuration.private_ip_address
+      controller    = "${element(azurerm_network_interface.vm_controller.*.ip_configuration.private_ip_address, count.index)}"
+      dc            = "${element(azurerm_network_interface.vm_dc.*.ip_configuration.private_ip_address, count.index)}"
+      rdsh          = "${element(azurerm_network_interface.vm_rdsh.*.ip_configuration.private_ip_address, count.index)}"
+      client        = "${element(azurerm_network_interface.vm_client.*.ip_configuration.private_ip_address, count.index)}"
+      byod          = "${element(azurerm_network_interface.vm_byod.*.ip_configuration.private_ip_address, count.index)}"
     }
   )
-  filename = "../ansible/inventory_test"
+  filename          = "../ansible/inventory_test"
 }

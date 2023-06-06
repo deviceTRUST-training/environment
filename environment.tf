e2aa646e7eb1b939084b81b826ed43ee560b86e8
Sven Jansen <sven.jansen@devicetrust.com>
2023-06-06 22:44:21 +0200

    .

--- environment.tf
+++ environment.tf
@@ -1,7 +1,8 @@
 # generate inventory file for Ansible
 resource "local_file" "hosts_cfg" {
+  count                = "${var.azure-environment.instance_count}"
   content = <<-EOT
-    %{ for ip in azurerm_network_interface.vm_byod.*.ip_configuration.private_ip_address ~}
+    %{ for ip in "${element(azurerm_network_interface.vm_byod.*.ip_configuration.private_ip_address, count.index)}" ~}
     ${ip} ansible_ssh_user=ec2-user
     %{ endfor ~}
   EOT

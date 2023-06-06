e133fda4bcd1b78b8c04bf6a20b7254a8c7abaaa
Sven Jansen <sven.jansen@devicetrust.com>
2023-06-06 22:24:44 +0200

    .

--- environment.tf
+++ environment.tf
@@ -5,7 +5,7 @@ resource "local_file" "hosts_cfg" {
       controller = azurerm_network_interface.vm_controller.*.ip_configuration.private_ip_address
       dc = azurerm_network_interface.vm_dc.*.ip_configuration.private_ip_address
       rdsh = azurerm_network_interface.vm_rdsh.*.ip_configuration.private_ip_address
-      client = azurerm_network_interface.vm_cient.*.ip_configuration.private_ip_address
+      client = azurerm_network_interface.vm_client.*.ip_configuration.private_ip_address
       byod = azurerm_network_interface.vm_byod.*.ip_configuration.private_ip_address
     }
   )

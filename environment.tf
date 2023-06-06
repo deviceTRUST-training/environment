36101a7f266bfbafa86911dc57f78f03e3c7088f
Sven Jansen <sven.jansen@devicetrust.com>
2023-06-06 22:50:14 +0200

    .

--- environment.tf
+++ environment.tf
@@ -1,6 +1,6 @@
 # generate inventory file for Ansible
 resource "local_file" "hosts_cfg" {
   count                = "${var.azure-environment.instance_count}"
-  content = "${element(azurerm_network_interface.vm_dc.*.ip_configuration.private_ip_address, count.index)}"
+  content = azurerm_network_interface.vm_dc.*.ip_configuration.private_ip_address[count.index]
   filename          = "../ansible/inventory_test"
 }

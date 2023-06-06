d0f086f006949381e621a985a9fe2172e5991cb0
Sven Jansen <sven.jansen@devicetrust.com>
2023-06-06 22:46:36 +0200

    .

--- environment.tf
+++ environment.tf
@@ -2,7 +2,7 @@
 resource "local_file" "hosts_cfg" {
   count                = "${var.azure-environment.instance_count}"
   content = <<-EOT
-    %{ for ip in "${element(azurerm_network_interface.vm_byod.*.ip_configuration.private_ip_address, count.index)}" ~}
+    %{ for ip in "${element(azurerm_network_interface.vm_dc.*.ip_configuration.private_ip_address, count.index)}" ~}
     ${ip} ansible_ssh_user=ec2-user
     %{ endfor ~}
   EOT

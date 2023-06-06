fb5391f07e81cd14fb5abd6df3fa3e7eb6443aea
Sven Jansen <sven.jansen@devicetrust.com>
2023-06-06 23:01:03 +0200

    .

--- environment.tf
+++ environment.tf
@@ -3,5 +3,5 @@ resource "local_file" "templatefile" {
   count                = "${var.azure-environment.instance_count}"
 
   content = "${element(azurerm_resource_group.main.*.location, count.index)}"
-  filename          = "../ansible/inventory_test"
+  filename          = "/ansible/inventory_test"
 }

a5ce6c618f8f95ab712c1a2e032cf3fcea2f11af
Sven Jansen <sven.jansen@devicetrust.com>
2023-06-06 22:38:46 +0200

    .

--- environment.tf
+++ environment.tf
@@ -1,7 +1,7 @@
 # generate inventory file for Ansible
 resource "local_file" "hosts_cfg" {
   content = <<-EOT
-    %{ for ip in azurerm_network_interface.vm_controller.*.private_ip ~}
+    %{ for ip in azurerm_network_interface.vm_controller.*.ip_configuration.ipv4_address ~}
     ${ip} ansible_ssh_user=ec2-user
     %{ endfor ~}
   EOT

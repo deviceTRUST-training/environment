baf15d88a4a8bf038553677fd0c061c84b1e30c6
Sven Jansen <sven.jansen@devicetrust.com>
2023-06-07 09:05:09 +0200

    .

---
+++ environment.tf
@@ -0,0 +1,13 @@
+# generate inventory file for Ansible
+resource "local_file" "hosts_cfg" {
+  content           = templatefile("${path.module}/ansible/templates/hosts.tpl",
+    {
+      controller    = "${azurerm_network_interface.vm_controller.*.private_ip_address}"
+      dc            = "${azurerm_network_interface.vm_dc.*.private_ip_address}"
+      rdsh          = "${azurerm_network_interface.vm_rdsh.*.private_ip_address}"
+      client        = "${azurerm_network_interface.vm_client.*.private_ip_address}"
+      byod          = "${azurerm_network_interface.vm_byod.*.private_ip_address}"
+    }
+  )
+  filename          = "../ansible/inventory_test"
+}

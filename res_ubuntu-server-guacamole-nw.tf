3f817510e0aa7bb5bcf7d7343e63bb9460421f7d
Sven Jansen <sven.jansen@devicetrust.com>
2023-06-07 10:31:44 +0200

    .

--- res_ubuntu-server-guacamole-nw.tf
+++ res_ubuntu-server-guacamole-nw.tf
@@ -7,7 +7,21 @@ resource "azurerm_public_ip" "vm_guacamole" {
   domain_name_label    = "${var.azure-environment.prefix}-guacamole"
 }
 
-resource "azurerm_network_interface" "vm_guacamole" {
+resource "azurerm_network_interface" "vm_guacamole_public" {
+  count               = "${var.azure-environment.instance_count}"
+  name                = "${var.azure-environment.prefix}_${count.index}_vm_guacamole_nic"
+  location            = azurerm_resource_group.infrastructure.location
+  resource_group_name = azurerm_resource_group.infrastructure.name
+  tags                = "${var.tags}"
+
+  ip_configuration {
+    name                          = "${var.azure-environment.prefix}_${count.index}_configuration"
+    subnet_id                     = "${element(azurerm_subnet.internal.*.id, count.index)}"
+    public_ip_address_id          = "${element(azurerm_public_ip.vm_guacamole.*.id, count.index)}"
+  }
+}
+
+resource "azurerm_network_interface" "vm_guacamole_private" {
   count               = "${var.azure-environment.instance_count}"
   name                = "${var.azure-environment.prefix}_${count.index}_vm_guacamole_nic"
   location            = azurerm_resource_group.infrastructure.location
@@ -19,6 +33,5 @@ resource "azurerm_network_interface" "vm_guacamole" {
     subnet_id                     = "${element(azurerm_subnet.internal.*.id, count.index)}"
     private_ip_address_allocation = "Static"
     private_ip_address            = "${var.azure-environment.ip_prefix}${count.index}.${var.vm.ip_guacamole}"
-    public_ip_address_id          = "${element(azurerm_public_ip.vm_guacamole.*.id, count.index)}"
   }
 }

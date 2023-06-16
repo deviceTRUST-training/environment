914b79c1f6e7e080514cf77cdef98abe2a192b9b
Sven Jansen <sven.jansen@devicetrust.com>
2023-06-16 11:56:26 +0200

    .

--- res_ubuntu-server-controller-nw.tf
+++ res_ubuntu-server-controller-nw.tf
@@ -1,4 +1,4 @@
-resource "azurerm_public_ip" "vm_controller" {
+resource "azurerm_public_ip" "vm_controller_main" {
   count                = "${var.azure-environment.instance_count}"
   name                 = "${var.azure-environment.prefix}_${count.index}_vm_controller_pip"
   location             = "${element(azurerm_resource_group.main.*.location, count.index)}"
@@ -8,7 +8,7 @@ resource "azurerm_public_ip" "vm_controller" {
   domain_name_label    = "${var.azure-environment.prefix}-ctrl-${count.index}"
 }
 
-resource "azurerm_network_interface" "vm_controller_1" {
+resource "azurerm_network_interface" "vm_controller_main" {
   count               = "${var.azure-environment.instance_count}"
   name                = "${var.azure-environment.prefix}_${count.index}_vm_controller_nic_1"
   location            = "${element(azurerm_resource_group.main.*.location, count.index)}"
@@ -20,21 +20,21 @@ resource "azurerm_network_interface" "vm_controller_1" {
     subnet_id                     = "${element(azurerm_subnet.main.*.id, count.index)}"
     private_ip_address_allocation = "Static"
     private_ip_address            = "${var.azure-environment.ip_prefix}${count.index}.${var.vm.ip_controller}"
-    public_ip_address_id          = "${element(azurerm_public_ip.vm_controller.*.id, count.index)}"
+    public_ip_address_id          = "${element(azurerm_public_ip.vm_controller_main.*.id, count.index)}"
   }
 }
 
-resource "azurerm_network_interface" "vm_controller_2" {
+resource "azurerm_network_interface" "vm_controller_infrastructure" {
   count               = "${var.azure-environment.instance_count}"
   name                = "${var.azure-environment.prefix}_${count.index}_vm_controller_nic_2"
-  location            = "${element(azurerm_resource_group.main.*.location, count.index)}"
-  resource_group_name = "${element(azurerm_resource_group.main.*.name, count.index)}"
+  location            = "${element(azurerm_resource_group.infrastructure.*.location, count.index)}"
+  resource_group_name = "${element(azurerm_resource_group.infrastructure.*.name, count.index)}"
   tags                = "${var.tags}"
 
   ip_configuration {
     name                          = "${var.azure-environment.prefix}_${count.index}_configuration"
-    subnet_id                     = "${element(azurerm_subnet.main.*.id, count.index)}"
+    subnet_id                     = azurerm_subnet.infrastructure.*.id
     private_ip_address_allocation = "Static"
-    private_ip_address            = "${var.azure-environment.ip_prefix}${count.index}.${var.vm.ip_controller}"
+    private_ip_address            = "${var.azure-environment.ip_prefix}100.${var.vm.ip_controller}"
   }
 }

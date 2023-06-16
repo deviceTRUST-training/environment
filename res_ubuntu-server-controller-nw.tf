678bf0e8efcb0529202ae252a119dfdfa455cc0a
Sven Jansen <sven.jansen@devicetrust.com>
2023-06-16 12:11:56 +0200

    .

--- res_ubuntu-server-controller-nw.tf
+++ res_ubuntu-server-controller-nw.tf
@@ -1,4 +1,4 @@
-resource "azurerm_public_ip" "vm_controller_main" {
+resource "azurerm_public_ip" "vm_controller" {
   count                = "${var.azure-environment.instance_count}"
   name                 = "${var.azure-environment.prefix}_${count.index}_vm_controller_pip"
   location             = "${element(azurerm_resource_group.main.*.location, count.index)}"
@@ -20,7 +20,6 @@ resource "azurerm_network_interface" "vm_controller_main" {
     subnet_id                     = "${element(azurerm_subnet.main.*.id, count.index)}"
     private_ip_address_allocation = "Static"
     private_ip_address            = "${var.azure-environment.ip_prefix}${count.index}.${var.vm.ip_controller}"
-    public_ip_address_id          = "${element(azurerm_public_ip.vm_controller_main.*.id, count.index)}"
   }
 }
 
@@ -35,7 +34,7 @@ resource "azurerm_network_interface" "vm_controller_infrastructure" {
     name                          = "${var.azure-environment.prefix}_${count.index}_configuration"
     subnet_id                     = azurerm_subnet.infrastructure.id
     private_ip_address_allocation = "Static"
-    # private_ip_address            = "${var.azure-environment.ip_prefix}100.${var.vm.ip_controller}"
     private_ip_address            = "${var.azure-environment.ip_prefix}100.1${count.index}"
+    public_ip_address_id          = "${element(azurerm_public_ip.vm_controller.*.id, count.index)}"
   }
 }

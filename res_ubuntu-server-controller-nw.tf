57bb16b66098cf1f10f4d2469b20311697958e57
Sven Jansen <sven.jansen@devicetrust.com>
2023-06-16 10:43:49 +0200

    .

--- res_ubuntu-server-controller-nw.tf
+++ res_ubuntu-server-controller-nw.tf
@@ -1,12 +1,3 @@
-resource "azurerm_public_ip" "vm_controller" {
-  count                = "${var.azure-environment.instance_count}"
-  name                 = "${var.azure-environment.prefix}_${count.index}_vm_controller_pip"
-  location             = "${element(azurerm_resource_group.main.*.location, count.index)}"
-  resource_group_name  = "${element(azurerm_resource_group.main.*.name, count.index)}"
-  allocation_method    = "Static"
-  tags                 = "${var.tags}"
-  domain_name_label    = "${var.azure-environment.prefix}-ctrl-${count.index}"
-}
 
 resource "azurerm_network_interface" "vm_controller" {
   count               = "${var.azure-environment.instance_count}"
@@ -20,6 +11,5 @@ resource "azurerm_network_interface" "vm_controller" {
     subnet_id                     = "${element(azurerm_subnet.internal.*.id, count.index)}"
     private_ip_address_allocation = "Static"
     private_ip_address            = "${var.azure-environment.ip_prefix}${count.index}.${var.vm.ip_controller}"
-    public_ip_address_id          = "${element(azurerm_public_ip.vm_controller.*.id, count.index)}"
   }
 }

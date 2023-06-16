de3f1f5f8e3d8b8a5e421883fd26e8d6666087f0
Sven Jansen <sven.jansen@devicetrust.com>
2023-06-16 13:37:03 +0200

    .

--- res_dependencies.tf
+++ res_dependencies.tf
@@ -1,27 +1,3 @@
-# Infrastructure resources
-
-resource "azurerm_resource_group" "infrastructure" {
-  name     = "${var.azure-environment.prefix}_infrastructure"
-  location = "${var.azure-environment.location}"
-  tags     = "${var.tags}"
-}
-
-resource "azurerm_virtual_network" "infrastructure" {
-  name                = "${var.azure-environment.prefix}_network_infrastructure"
-  address_space       = ["${var.azure-environment.ip_prefix}100.0/24"]
-  location            = azurerm_resource_group.infrastructure.location
-  resource_group_name = azurerm_resource_group.infrastructure.name
-  tags                = "${var.tags}"
-}
-
-resource "azurerm_subnet" "infrastructure" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.infrastructure.name
-  virtual_network_name = azurerm_virtual_network.infrastructure.name
-  address_prefixes     = ["${var.azure-environment.ip_prefix}100.0/24"]
-}
-
-# VM resources
 resource "azurerm_resource_group" "main" {
   count    = "${var.azure-environment.instance_count}"
   name     = "${var.azure-environment.prefix}_resources_${count.index}"
@@ -29,20 +5,37 @@ resource "azurerm_resource_group" "main" {
   tags     = "${var.tags}"
 }
 
-resource "azurerm_virtual_network" "main" {
+resource "azurerm_virtual_network" "internal" {
   count               = "${var.azure-environment.instance_count}"
   name                = "${var.azure-environment.prefix}_network_${count.index}"
   address_space       = ["${var.azure-environment.ip_prefix}${count.index}.0/24"]
-  location            = "${element(azurerm_resource_group.main.*.location, count.index)}"
-  resource_group_name = "${element(azurerm_resource_group.main.*.name, count.index)}"
+  location            = "${element(azurerm_resource_group.internal.*.location, count.index)}"
+  resource_group_name = "${element(azurerm_resource_group.internal.*.name, count.index)}"
   tags                = "${var.tags}"
   dns_servers         = [ "${var.azure-environment.ip_prefix}${count.index}.${var.vm.ip_dc}" ]
 }
 
-resource "azurerm_subnet" "main" {
+resource "azurerm_subnet" "internal" {
   count                = "${var.azure-environment.instance_count}"
   name                 = "internal"
-  resource_group_name  = "${element(azurerm_resource_group.main.*.name, count.index)}"
-  virtual_network_name = "${element(azurerm_virtual_network.main.*.name, count.index)}"
+  resource_group_name  = "${element(azurerm_resource_group.internal.*.name, count.index)}"
+  virtual_network_name = "${element(azurerm_virtual_network.internal.*.name, count.index)}"
   address_prefixes     = ["${var.azure-environment.ip_prefix}${count.index}.0/24"]
+}
+
+resource "azurerm_virtual_network" "external" {
+  count               = "${var.azure-environment.instance_count}"
+  name                = "${var.azure-environment.prefix}_network_${count.index}"
+  # address_space       = ["${var.azure-environment.ip_prefix}${count.index}.0/24"]
+  location            = "${element(azurerm_resource_group.external.*.location, count.index)}"
+  resource_group_name = "${element(azurerm_resource_group.external.*.name, count.index)}"
+  tags                = "${var.tags}"
+}
+
+resource "azurerm_subnet" "external" {
+  count                = "${var.azure-environment.instance_count}"
+  name                 = "internal"
+  resource_group_name  = "${element(azurerm_resource_group.external.*.name, count.index)}"
+  virtual_network_name = "${element(azurerm_virtual_network.external.*.name, count.index)}"
+  # address_prefixes     = ["${var.azure-environment.ip_prefix}${count.index}.0/24"]
 }

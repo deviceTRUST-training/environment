a614a0f20e49d793882058159e6f3b90df2541a4
Sven Jansen <sven.jansen@devicetrust.com>
2023-06-16 12:41:17 +0200

    .

--- res_ubuntu-server-controller.tf
+++ res_ubuntu-server-controller.tf
@@ -8,7 +8,10 @@ resource "azurerm_virtual_machine" "vm_controller" {
   name                  = "${var.azure-environment.prefix}_${count.index}_vm_controller"
   location              = "${element(azurerm_resource_group.main.*.location, count.index)}"
   resource_group_name   = "${element(azurerm_resource_group.main.*.name, count.index)}"
-  network_interface_ids = ["${element(azurerm_network_interface.vm_controller_main.*.id, count.index)}", "${element(azurerm_network_interface.vm_controller_infrastructure.*.id, count.index)}"]
+  network_interface_ids = [
+    "${element(azurerm_network_interface.vm_controller_main.*.id, count.index)}", 
+    "${element(azurerm_network_interface.vm_controller_infrastructure.*.id, count.index)}"
+  ]
   vm_size               = "Standard_B1ms"
 
   delete_os_disk_on_termination = true

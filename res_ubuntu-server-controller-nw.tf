adcb8c40e981a2c3c7699a5e4bc3064177b9a941
Sven Jansen <sven.jansen@devicetrust.com>
2023-06-16 13:50:46 +0200

    .

--- res_ubuntu-server-controller-nw.tf
+++ res_ubuntu-server-controller-nw.tf
@@ -8,31 +8,31 @@ resource "azurerm_public_ip" "vm_controller" {
   domain_name_label    = "${var.azure-environment.prefix}-ctrl-${count.index}"
 }
 
-resource "azurerm_network_interface" "vm_controller_main" {
+resource "azurerm_network_interface" "vm_controller_internal" {
   count               = "${var.azure-environment.instance_count}"
-  name                = "${var.azure-environment.prefix}_${count.index}_vm_controller_nic_main"
-  location            = "${element(azurerm_resource_group.main.*.location, count.index)}"
-  resource_group_name = "${element(azurerm_resource_group.main.*.name, count.index)}"
+  name                = "${var.azure-environment.prefix}_${count.index}_vm_controller_nic_internal"
+  location            = "${element(azurerm_resource_group.internal.*.location, count.index)}"
+  resource_group_name = "${element(azurerm_resource_group.internal.*.name, count.index)}"
   tags                = "${var.tags}"
 
   ip_configuration {
     name                          = "${var.azure-environment.prefix}_${count.index}_configuration"
-    subnet_id                     = "${element(azurerm_subnet.main.*.id, count.index)}"
+    subnet_id                     = "${element(azurerm_subnet.internal.*.id, count.index)}"
     private_ip_address_allocation = "Static"
     private_ip_address            = "${var.azure-environment.ip_prefix}${count.index}.${var.vm.ip_controller}"
   }
 }
 
-resource "azurerm_network_interface" "vm_controller_infrastructure" {
+resource "azurerm_network_interface" "vm_controller_external" {
   count               = "${var.azure-environment.instance_count}"
-  name                = "${var.azure-environment.prefix}_${count.index}_vm_controller_nic_infrastructure"
-  location            = azurerm_resource_group.infrastructure.location
-  resource_group_name = azurerm_resource_group.infrastructure.name
+  name                = "${var.azure-environment.prefix}_${count.index}_vm_controller_nic_external"
+  location            = azurerm_resource_group.external.location
+  resource_group_name = azurerm_resource_group.external.name
   tags                = "${var.tags}"
 
   ip_configuration {
     name                          = "${var.azure-environment.prefix}_${count.index}_configuration"
-    subnet_id                     = azurerm_subnet.infrastructure.id
+    subnet_id                     = azurerm_subnet.external.id
     private_ip_address_allocation = "Static"
     private_ip_address            = "${var.azure-environment.ip_prefix}100.1${count.index}"
     public_ip_address_id          = "${element(azurerm_public_ip.vm_controller.*.id, count.index)}"

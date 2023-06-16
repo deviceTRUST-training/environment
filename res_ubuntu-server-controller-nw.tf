5df25c04fd0fb25293ecadd158a703e09bd837a2
Sven Jansen <sven.jansen@devicetrust.com>
2023-06-16 15:40:50 +0200

    .

--- res_ubuntu-server-controller-nw.tf
+++ res_ubuntu-server-controller-nw.tf
@@ -16,7 +16,7 @@ resource "azurerm_network_interface" "vm_controller" {
   tags                = "${var.tags}"
   ip_configuration {
     name                          = "${var.azure-environment.prefix}_${count.index}_configuration"
-    subnet_id                     = "${element(azurerm_subnet.external.*.id, count.index)}"
+    subnet_id                     = "${element(azurerm_subnet.main.*.id, count.index)}"
     private_ip_address_allocation = "Static"
     private_ip_address            = "${var.azure-environment.ip_prefix}${count.index}.${var.vm.ip_controller}"
     public_ip_address_id          = "${element(azurerm_public_ip.vm_controller.*.id, count.index)}"

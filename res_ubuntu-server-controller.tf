9b3c45052fd096931bea9c8e0e879be636e23a56
Sven Jansen <sven.jansen@devicetrust.com>
2023-06-20 11:04:14 +0200

    .

--- res_ubuntu-server-controller.tf
+++ res_ubuntu-server-controller.tf
@@ -63,16 +63,16 @@ resource "azurerm_virtual_machine" "vm_controller" {
     ]
   }
 
-#  provisioner "remote-exec" {
-#    inline = [
-#      "cd /tmp",
-#      "cd /tmp/ansible",
-#      "ansible-galaxy install -v -r requirements.yml -p ./roles/",
-#      "ansible-playbook -i inventory site.yml --extra-vars 'ansible_user=${var.vm.username} ansible_password=${var.vm.password} domain_admin_user=${var.vm.username}@${var.vm.domain_dns_name} domain_admin_password=${var.vm.password} safe_mode_password=${var.vm.password}'",
-#    ]
-#  }
+  provisioner "remote-exec" {
+    inline = [
+      "cd /tmp",
+      "cd /tmp/ansible",
+      "ansible-galaxy install -v -r requirements.yml -p ./roles/",
+      "ansible-playbook -i inventory site.yml --extra-vars 'ansible_user=${var.vm.username} ansible_password=${var.vm.password} domain_admin_user=${var.vm.username}@${var.vm.domain_dns_name} domain_admin_password=${var.vm.password} safe_mode_password=${var.vm.password}'",
+    ]
+  }
 
-  # depends_on = [azurerm_virtual_machine.vm_dc, azurerm_virtual_machine.vm_rdsh, azurerm_virtual_machine.vm_client, azurerm_virtual_machine.vm_byod]
+  depends_on = [azurerm_virtual_machine.vm_dc, azurerm_virtual_machine.vm_rdsh, azurerm_virtual_machine.vm_client, azurerm_virtual_machine.vm_byod]
 
   tags = "${var.tags}"
 }

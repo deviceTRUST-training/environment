bfe1f3bb7998c2c184078b0f94a4e9db58192f12
Sven Jansen <sven.jansen@devicetrust.com>
2023-08-01 16:58:18 +0200

    .

--- res_ubuntu-server-controller.tf
+++ res_ubuntu-server-controller.tf
@@ -59,7 +59,6 @@ resource "azurerm_virtual_machine" "vm_controller" {
   }
 
   provisioner "remote-exec" {
-    on_failure = continue
     inline = [
       "sudo apt-get install -y unzip git ansible"
     ]

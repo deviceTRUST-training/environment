22148f04fffc28484a319a71a5c4d20dfa86cdcc
Sven Jansen <sven.jansen@devicetrust.com>
2023-08-02 17:28:03 +0200

    .

--- res_ubuntu-server-guacamole-nw.tf
+++ res_ubuntu-server-guacamole-nw.tf
@@ -74,7 +74,17 @@ resource "azurerm_network_security_group" "vm_guacamole" {
       source_address_prefix  = "92.50.117.117/32"
       destination_address_prefix = "*"
     }
-    
+   security_rule {
+      name                   = "in_guacamole_sven"
+      priority               = 212
+      direction              = "Inbound"
+      access                 = "Allow"
+      protocol               = "Tcp"
+      source_port_range      = "*"
+      destination_port_range = "8080"
+      source_address_prefix  = "92.50.117.117/32"
+      destination_address_prefix = "*"
+    }   
 }
 
 resource "azurerm_network_interface_security_group_association" "vm_guacamole_external" {

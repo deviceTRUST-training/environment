f32f7c60eba395e84d04fc3aedf3e08cc4222f0b
Sven Jansen <sven.jansen@devicetrust.com>
2023-08-02 17:00:47 +0200

    .

--- res_ubuntu-server-guacamole-nw.tf
+++ res_ubuntu-server-guacamole-nw.tf
@@ -17,7 +17,7 @@ resource "azurerm_network_interface" "vm_guacamole_external" {
     name                          = "${var.azure-environment.prefix}_configuration"
     subnet_id                     = azurerm_subnet.external_guac.id
     private_ip_address_allocation = "Static"
-    private_ip_address            = "10.10.254.${var.vm.ip_guacamole}"
+    private_ip_address            = "10.10.252.${var.vm.ip_guacamole}"
     public_ip_address_id          = azurerm_public_ip.vm_guacamole.id
   }
 }
@@ -32,7 +32,7 @@ resource "azurerm_network_interface" "vm_guacamole_internal" {
     name                          = "${var.azure-environment.prefix}_configuration"
     subnet_id                     = azurerm_subnet.guac.id
     private_ip_address_allocation = "Static"
-    private_ip_address            = "10.10.253.${var.vm.ip_guacamole}"
+    private_ip_address            = "10.10.251.${var.vm.ip_guacamole}"
   }
 }
 

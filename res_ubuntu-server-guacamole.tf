9d59871fab9c0e1cfd3699c15901571e09697c8b
Sven Jansen <sven.jansen@devicetrust.com>
2023-08-03 16:24:51 +0200

    .

--- res_ubuntu-server-guacamole.tf
+++ res_ubuntu-server-guacamole.tf
@@ -49,9 +49,6 @@ resource "azurerm_virtual_machine" "vm_guacamole" {
     inline = [
       "sleep 5s",
       "sudo apt -y update",
-      "sudo DEBIAN_FRONTEND=noninteractive apt -y upgrade",
-      "sudo DEBIAN_FRONTEND=noninteractive apt -y dist-upgrade",
-      "sudo apt -y autoremove",
       "sleep 5s",
       "sudo apt -y install docker docker-compose git"
     ]

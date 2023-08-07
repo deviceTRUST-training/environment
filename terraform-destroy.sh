77bb95aee7716700d301d0c4a9646927d1678ab0
Sven Jansen <sven.jansen@devicetrust.com>
2023-08-07 16:39:50 +0200

    .

---
+++ terraform-destroy.sh
@@ -0,0 +1,18 @@
+terraformdestroy(){
+    if terraform destroy
+    then # destroy executed successfully
+         echo >&2 "Successfully destroyed!"
+         retval=1
+    else
+         echo >&2 "destroy failed!"
+         retval=0
+    fi
+    return "$retval"
+}
+
+retval=0
+
+while [ "$retval" == 0 ]
+do
+  terraformdestroy
+done

e093c1ce4c126d5583f9622ee0ad84090c12ba50
Sven Jansen <sven.jansen@devicetrust.com>
2023-08-15 09:37:43 +0200

    .

--- ansible/create-env-guacamole.sh
+++ ansible/create-env-guacamole.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ansible-galaxy role install -v -r requirements.yml -p roles/
-ansible-galaxy collection install -v -r requirements.yml
+# ansible-galaxy collection install -v -r requirements.yml
 ansible-playbook -i inventory create-env-guacamole.yml

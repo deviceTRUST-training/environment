8b5ab767bdcadd4ecb46119b8193eced385bb68e
Sven Jansen <sven.jansen@devicetrust.com>
2023-08-21 11:03:58 +0200

    .

--- res_windows-server-dc-nw.tf
+++ res_windows-server-dc-nw.tf
@@ -7,6 +7,6 @@ resource "azurerm_network_interface" "vm_dc_internal" {
     name                          = "${var.azure-environment.prefix}_configuration"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Static"
-    private_ip_address            = "10.10.${count.index}.${var.vm.ip_dc}"
+    private_ip_address            = "10.10.0.${var.vm.ip_dc}"
   }
 }

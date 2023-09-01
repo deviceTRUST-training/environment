fc6ebf864b86e1745d638d350a3ec0448e75942f
Sven Jansen <sven.jansen@devicetrust.com>
2023-09-01 11:08:27 +0200

    .

--- environment.tf
+++ environment.tf
@@ -3,9 +3,9 @@ resource "local_file" "hosts_cfg" {
   content           = templatefile("${path.module}/ansible/templates/hosts.tpl",
     {
       dc            = "${azurerm_network_interface.vm_dc.*.private_ip_address}"
-      rdsh          = "${azurerm_network_interface.vm_rdsh_internal.*.private_ip_address}"
-      client        = "${azurerm_network_interface.vm_client_internal.*.private_ip_address}"
-      byod          = "${azurerm_network_interface.vm_byod_internal.*.private_ip_address}"
+      rdsh          = "${azurerm_network_interface.vm_rdsh.*.private_ip_address}"
+      client        = "${azurerm_network_interface.vm_client.*.private_ip_address}"
+      byod          = "${azurerm_network_interface.vm_byod.*.private_ip_address}"
     }
   )
   filename          = "${path.module}/ansible/inventory"

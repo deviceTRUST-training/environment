8d6e0e647a7bd1914fd2de33aa6df25dda9a425d
Sven Jansen <sven.jansen@devicetrust.com>
2023-09-01 15:43:36 +0200

    .

--- environment.tf
+++ environment.tf
@@ -2,10 +2,10 @@
 resource "local_file" "hosts_cfg" {
   content           = templatefile("${path.module}/templates/hosts.tpl",
     {
-      dc            = "${azurerm_network_interface.vm_dc.*.private_ip_address} hostname = dc${azurerm_network_interface.vm_dc.*.index}"
-      rdsh          = "${azurerm_network_interface.vm_rdsh.*.private_ip_address} hostname = rdsh${azurerm_network_interface.vm_dc.*.index}"
-      client        = "${azurerm_network_interface.vm_client.*.private_ip_address} hostname = client${azurerm_network_interface.vm_dc.*.index}"
-      byod          = "${azurerm_network_interface.vm_byod.*.private_ip_address} hostname = byod${azurerm_network_interface.vm_dc.*.index}"
+      dc            = "${azurerm_network_interface.vm_dc.*.private_ip_address} hostname = dc${azurerm_network_interface.vm_dc.*.count}"
+      rdsh          = "${azurerm_network_interface.vm_rdsh.*.private_ip_address} hostname = rdsh${azurerm_network_interface.vm_dc.*.count}"
+      client        = "${azurerm_network_interface.vm_client.*.private_ip_address} hostname = client${azurerm_network_interface.vm_dc.*.count}"
+      byod          = "${azurerm_network_interface.vm_byod.*.private_ip_address} hostname = byod${azurerm_network_interface.vm_dc.*.count}"
     }
   )
   filename          = "${path.module}/ansible/inventory"

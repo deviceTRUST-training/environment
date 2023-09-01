961a16bbefd7391b81b104b1a25b8b99d1749867
Sven Jansen <sven.jansen@devicetrust.com>
2023-09-01 11:07:35 +0200

    .

--- res_windows-client-byod-nw.tf
+++ res_windows-client-byod-nw.tf
@@ -1,13 +1,13 @@
-resource "azurerm_network_interface" "vm_byod_internal" {
+resource "azurerm_network_interface" "vm_byod" {
   count               = "${var.azure-environment.instance_count}"
-  name                = "${var.azure-environment.prefix}_${count.index}_vm_byod_nic_internal"
+  name                = "${var.azure-environment.prefix}_${count.index}_vm_byod_nic"
   location            = "${element(azurerm_resource_group.training.*.location, count.index)}"
   resource_group_name = "${element(azurerm_resource_group.training.*.name, count.index)}"
   tags                = "${var.tags}"
   ip_configuration {
     name                          = "${var.azure-environment.prefix}_${count.index}_configuration"
-    subnet_id                     = "${element(azurerm_subnet.internal.*.id, count.index)}"
+    subnet_id                     = "${element(azurerm_subnet.member.*.id, count.index)}"
     private_ip_address_allocation = "Static"
-    private_ip_address            = "10.${count.index}.2.${var.vm.ip_byod}"
+    private_ip_address            = "10.10.10.${var.vm.ip_byod + (count.index * 10)}"
   }
 }

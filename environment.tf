4c91acbb361d2161676e969ad0509d4f34c0d1cb
Sven Jansen <sven.jansen@devicetrust.com>
2023-09-07 12:04:48 +0200

    .

--- environment.tf
+++ environment.tf
@@ -3,6 +3,7 @@ resource "local_file" "hosts_cfg" {
   content           = templatefile("${path.module}/templates/hosts.tpl",
     {
       dc            = "${azurerm_network_interface.vm_dc.*.private_ip_address}"
+      guac          = "${azurerm_network_interface.vm_guacamole.*.private_ip_address}"
       rdsh          = "${azurerm_network_interface.vm_rdsh.*.private_ip_address}"
       client        = "${azurerm_network_interface.vm_client.*.private_ip_address}"
       byod          = "${azurerm_network_interface.vm_byod.*.private_ip_address}"
